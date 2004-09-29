e94fae0f2f4afff4e0853dde64119cb9e44b8168
paarlber <paarlber@dedd6328-c50f-0410-8be5-b54a49638459>
2004-09-29 20:12:50 +0000

    Updated version of tdisk.f (dated May 25, 2003).
    git-svn-id: svn+ssh://bastion.mars.asu.edu/mars/tes/svn/dev/krc/trunk@4288 dedd6328-c50f-0410-8be5-b54a49638459

--- tdisk.f
+++ tdisk.f
@@ -19,10 +19,10 @@ C_Lims
         INTEGER MD21,MD22,MD23,MD24,MD25 ! maximum dimensions
         PARAMETER (MD21=MAXNH)	! hours
         PARAMETER (MD22=2)	! surface & planetary
-        PARAMETER (MD23=10)	! latitudes
+        PARAMETER (MD23=19)	! latitudes
         PARAMETER (MD24=80)	! seasons
         PARAMETER (MD25=10)	! cases = inertias
-        REAL*4 BMD2 (MD21,MD22,MD23,MD24,MD25) ! 384000 words
+        REAL*4 BMD2 (MD21*MD22*MD23*MD24*MD25) ! 729600 words
 
         INTEGER MD31,MD34,MD35! maximum dimensions
         PARAMETER (MD31=1+2*MAXNH+2*MAXN1+MAXNH) ! combined
@@ -41,23 +41,33 @@ C	also, sets record position to append on next write
 C  4 = close the file
 C  5 = write a record of  KRCCOM
 	INTEGER KREC	!in (when  KODE=1): file status: 0=new  1=old
+C_Desc
+C Can write several styles of binary files; controled by K4OUT. See helplist.txt
+C The two basic families are Direct access files containing combinations of
+C the COMMON arrays, and bin5 files of specific variables and arrays.
+C The bin5 files have K4OUT=50+; the parameter statements above set the 
+C maximum allowed sizes, but they are written dense to the requested array
+C sizes, and BINF5 writes only the utilized part of the storage allocated.
+C
 C_Hist ~1975 Hugh Kieffer Original version; evolved over 20 years
 C 93mar03 ecisneros ported code to unix platform
 C		          converted include filenames to lowercase.
 C 97feb11  HHK get file name only from common
 C 97aug03  HHK add short form output  97sep08 accomodate  TPF,ALAT,ELEV
 C 98may26  HHK  NRECL in bytes for  Linux
-C 98aug31  HHK include custom  bin5file capablity 
+C 98aug31  HHK include custom  bin5 file capablity 
 C                   51=(30 layers, 2 min/max,  5 lat, 40 seasons,  5 cases)
 C     The last 2 layers for min are: TpMin, L_sub_s ; for max are: TpMax, DJU5
 C 98nov16  HHK add  52=(24 hours,   Tsurf/Tp, 10 lat, 80 seasons, 10 cases)
 C 99nov24  HHK move setting  LOPN2 false at 400
 C 2002aug04 HHK Add 53=(combo  at 1 lat, 2+80 seasons, 10 cases). Recode logic
 C 2002aug15 HK Increase season size
+C 2003may25 HK Increase MD23 from 10 to 19. Use NDx consistently for K4OUT=52
 C_Bugs
-C all but last dimension of  BMDn idealy would be dynamically allocated
+C all but last dimension of  BMDn ideally would be dynamically allocated
 C_End6789012345678901234567890123456789012345678901234567890123456789012_4567890
-C Direct write fillout will zero-fill unused part of record
+
+C Direct write will zero-fill unused part of record
 	CHARACTER CSTAT*3
 	REAL*4 COMKRC(NWKRC),COMLAT(NWLAT),COMDAY(NWDAY)
 	EQUIVALENCE (COMKRC,ALB),(COMLAT,NDJ4),(COMDAY,X)
@@ -65,9 +75,12 @@ C Direct write fillout will zero-fill unused part of record
         INTEGER*4 JB5(10) ! sizes to go to  BINF5
         CHARACTER*30 HEADER /'KRC-tes custom save'/
         INTEGER HEADLEN /30/
-        INTEGER JREC,NWTOT,NRECL,I,IOS,J,k,ND1,ND3,IRET
+        INTEGER JREC,NWTOT,NRECL,I,IOS,J,k,ND1,ND3,ND4,IRET
 	INTEGER MM2,MM3,MM4	! multiple array dimensions
+
 	REAL*4 ASOLH(MAXNH),AH
+
+	SAVE JB5,ND1,ND3,ND4,ND5,JREC,MM2,MM3,MM4 ! insure these remain defined
 C
 C	write(iosp,*)'TDISK called with kode,case,j5=',kode,case,j5
 	GOTO (100,200,300,400,500),KODE
@@ -85,21 +98,20 @@ CC	  WRITE (IOPM,*) ' File for saving results?'
 C     recl may be bytes or longwords, depending upon  OS and compiler options.
 C Solaris: it is longwords, since file is unformatted (default for direct access)
 	IF (K4OUT.LE.50) THEN	! Open output file
-	  IF (K4OUT.LT.0) THEN	! k4out is negative
+	  IF (K4OUT.LT.0) THEN	!  K4OUT is negative
 	    NWTOT=2*MACNH*MAXN4 !  TSF  &  TPF only
 	  ELSEIF (K4OUT.EQ.0) THEN
-	    NWTOT=NWKRC+NWLAT	! krccom & latcom
-	  ELSE			! k4out is 1:50
-	    NWTOT=NWKRC+NWDAY	! krccom & daycom
+	    NWTOT=NWKRC+NWLAT	!  KRCCOM & LATCOM
+	  ELSE			!  K4OUT is 1:50
+	    NWTOT=NWKRC+NWDAY	!  KRCCOM & DAYCOM
 	  ENDIF
-	  NRECL=4*NWTOT		!  OR nrecl=nwtot  ! depends upon compiler <<<<
+	  NRECL=4*NWTOT		!  or  NRECL=NWTOT  ! depends upon compiler <<<<
 	  OPEN (UNIT=IOD2,FILE=FDISK,ACCESS='DIRECT',STATUS=CSTAT
 	1      ,RECL=NRECL,ERR=191,IOSTAT=IOS)
 	  WRITE(IOSP,110)CSTAT,FDISK,NWTOT, NRECL,DAYTIM
 	  WRITE(IOERR,110)CSTAT,FDISK,NWTOT, NRECL,DAYTIM
  110	  FORMAT (/'0TDISK:  Opened ',A,' direct access file = ',A
 	1      /8X,'Record length in R*4 & NRECL = ',2I6,3X,'NOW = ',5A4)
-	  JREC=0
 	ELSE			! setup for BIN5 file, which does not use  JREC
 	  JB5(5) = 0		!
 	  JB5(6) = 0		!
@@ -116,15 +128,8 @@ C Solaris: it is longwords, since file is unformatted (default for direct access
 	    MM2=MD11*MD12
 	    MM3=MM2*MD13
 	    MM4=MM3*MD14
-	  ELSEIF (K4OUT.EQ.52) THEN 
-	    JB5(1) = 5		! # of dimensions
-	    JB5(2) = MD21	!
-	    JB5(3) = MD22	!
-	    JB5(4) = MD23	!
-	    JB5(5) = MD24	!
-	    JB5(6) = MD25	!
-	  ELSE			! K4OUT=53
-	    if (N4.ne.1) WRITE(iosp,*)'ERROR: Expect J4=1 for K4OUT=53'
+	  ELSEIF (K4OUT.EQ.53) THEN 
+	    IF (N4.NE.1) WRITE(IOSP,*)'ERROR: Expect J4=1 for K4OUT=53'
 	    JB5(1) = 3		! # of dimensions
 	    JB5(2) = MD31	!
 	    JB5(3) = MD34	!
@@ -134,6 +139,7 @@ C Solaris: it is longwords, since file is unformatted (default for direct access
 	   WRITE(IOSP,13)K4OUT,JB5
  13	   FORMAT ('Initiated custom output: K4OUT=',I3,/'JB5=',10I6)
 	ENDIF
+	JREC=0
 	LOPN2=.TRUE.
 	GOTO 9
 C
@@ -178,7 +184,38 @@ C write next record (internal record count)		2  2  2  2  2  2  2  2
 	      BMD1(I+MD11-1) =DJU5
 	    ENDDO
 	  ENDIF
-	ELSEIF (K4OUT.EQ.53) THEN !......................................
+	ELSEIF (K4OUT.EQ.52) THEN !......................................
+	  K5=J5-JDISK+1		! 1-based season count after start to disk
+	  IF (JREC.EQ.0) THEN 
+	    ND1=N24		! size to be used for first dimension
+	    ND3=N4		!  " 3rd
+	    ND4=N5-JDISK+1	!  " 4th
+	    MM2=ND1*2		! total size through 2nd dimension
+	    MM3=MM2*ND3		!  " 3rd
+	    MM4=MM3*ND4		!  " 4th
+	    JB5(1) = 5		! # of dimensions
+	    JB5(2) = ND1	!
+	    JB5(3) = MD22	!
+	    JB5(4) = ND3	!
+	    JB5(5) = ND4	!
+	  ELSE
+	    IF (N24.NE.ND1 .OR. N4.NE.ND3 .OR. N5-JDISK+1.NE.ND4) THEN
+	      WRITE (IOERR,*)
+	1 'Change of NHOUR, NLAT, or #toDisk not allowed for K4OUT=52'
+	      GOTO 9
+	    ENDIF
+	  ENDIF
+	  JREC=JREC+1
+	  IF (K5. LE.MD24 .AND. NCASE.LE.MD25) THEN
+	    I=MM4*(NCASE-1)+ MM3*(K5-1) + 1 ! address of first word to write
+	    DO J4=1,ND3		! do each latitude
+	      CALL R2R (TSF(1,J4),BMD1(I),ND1)
+	      I=I+ND1
+	      CALL R2R (TPF(1,J4),BMD1(I),ND1)
+	      I=I+ND1
+	    ENDDO
+	  ENDIF
+	ELSE	! K4OUT=53  ...................................
 	  IF  (NCASE.LE.MD35) THEN
 	    JREC=J5-JDISK
 	    IF (JREC.EQ.1) THEN
@@ -215,15 +252,6 @@ C	      CALL R2R (TOUT,BMD3(I),MAXN2)
 C	      write(iosp,*)'case,j5,I=',case,j5,I
 	    ENDIF
 	  ENDIF
-	ELSE			! K4OUT=52  ...................................
-	  IF (J5.LE.MD24 .AND. NCASE.LE.MD25) THEN
-	    ND1=MIN(N24,MD21)
-	    ND3=MIN(N4,MD23)
-	    DO J4=1,ND3		! do each latitude
-	      CALL R2R (TSF(1,J4),BMD2(1,1,J4,J5,NCASE),ND1)
-	      CALL R2R (TPF(1,J4),BMD2(1,2,J4,J5,NCASE),ND1)
-	    ENDDO
-	  ENDIF
 	ENDIF			!...............................................
 	GOTO 9
 C
@@ -243,6 +271,7 @@ C
 C close the file					4  4  4  4  4  4  4  4
  400	IF (LOPN2) then
 	  IF (K4OUT.GT.50) THEN ! save custom array
+	    IF (K4OUT.EQ.52) JB5(6) = NCASE 
 	    WRITE (IOSP,*)'jb5=',JB5
 	    CALL BINF5 ('W',FDISK,HEADER,JB5,BMD1,IRET) ! others equivalenced
 	    WRITE (IOSP,*)'Wrote custom array, iret=',IRET
