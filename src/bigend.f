d565374fe11e80283fbaa3fd74aea4d8a71bf103
randy <randy@dedd6328-c50f-0410-8be5-b54a49638459>
2013-03-27 00:29:17 +0000

    bigend integrated into build procedure, using Hugh's F77 compatible version of bigend, and the bin5 fixes to make it all work correctly. As far as I know this represents the latest working version of krc as of March 26, 2013.
    git-svn-id: svn+ssh://bastion.mars.asu.edu/mars/tes/svn/dev/krc/trunk@11864 dedd6328-c50f-0410-8be5-b54a49638459

--- src/bigend.f
+++ src/bigend.f
@@ -1,16 +1,27 @@
-
-	INTEGER FUNCTION BIGEND ()
-C_Title  Determine if hardware we are on is big or little endian
-C_Arguments
-C_Calls 0
-C_History  20130130  Randy Kaelber original version
+      INTEGER FUNCTION BIGEND ()
+C_Title  BIGEND  Determine if hardware byte order is big or little endian 
+C_Arguments  none
+C Function  Integer: 1 if big-endian    0 if little endian  
+C -1=neither  2=both  These results should never ever occur!
+C 
+C_Calls  none
+C_Desc  Overlaps in memory a 4-byte integer and 4 bytes
+C_History 2013feb02 Hugh Kieffer Original version
 C_end
-C
+C      
+      IMPLICIT NONE  
+ 
+      INTEGER*1 B(4)
+      INTEGER*4 I
+      EQUIVALENCE (I,B(1))      ! overlap B and I
 
-        BIGEND = 0
-        if (ichar(transfer(1,'a')) == 0) then
-            BIGEND = 1
-        endif
+      I=1                       ! set one bit of 32-bit word
+      BIGEND=-1                 ! set as undetermined
+      IF (B(4) .EQ. 1) BIGEND = 1    ! least-significant byte is the last
+      IF (B(1) .EQ. 1) BIGEND = BIGEND+1 ! least-sig. byte is first
+C              If both tests true, yields +2 . Virtually impossible
 
-        RETURN
-	END
+C      write(*,*) B  ! debug test 
+      
+      RETURN
+      END
