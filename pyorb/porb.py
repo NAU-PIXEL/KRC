373eea34ffaf69825568624b5c2ac0ce6182f30b
nmsplanets <nathan.smith@nau.edu>
2025-11-21 20:14:28 +0000

    broke out porb.py into functions, pulled constants into constants.py

--- pyorb/porb.py
+++ pyorb/porb.py
@@ -7,48 +7,20 @@
 import numpy as np
 import spiceypy as spice
 import datetime
+import constants as const
 
-def get_orbit_params(body_name, body_naifid):
+def get_orbital_elements(metakernel, body_naifid, epoch_date):
     '''
-    Determines the orbital parameters of a body based on spice kernels.
-    Outputs a dictionary containing all the variables to include in the standard PORB
-    input table for KRC.
+    Use spice to get the keplerian(?) orbital elements for a specified body.
+    returns a tuple of floats:
+    long_of_asc_node:   longitude of the ascending node [radians]
+    eccentricity:       eccentricity [unitless]
+    inclination:        inclination [radians]
+    arg_of_peri:        argument of perihelion [radians] 
+    mean_anomaly:       mean anomaly at epoch [radians]
+    semimajor_axis:     semimajor axis [km]
+    epoch_JD:           Julian date of epoch [Julian Date]
     '''
-
-    out_dict={}
-
-    # PORB version date:
-    out_dict['porb_version'] = '2025nov20'
-    # generation date:
-    out_dict['generation_date'] = datetime.datetime.now().strftime('%Y %b %d %H:%M:%S')
-    # epoch at which to calculate orbital params (must be covered by available kernels)
-    epoch_date = datetime.datetime(2025,1,1,0,0,0)
-    
-
-    ##### Unused flags #####
-    # SFLAG : Input system flag 0:default, +1: pole=ec +10:orb=eq
-    out_dict['SFLAG']   = 0
-    # PBUG  : Debug Flag
-    out_dict['PBUG']    = 0
-    # spar17: unused
-    out_dict['spar17']  = 0
-    # spar21: unused
-    out_dict['spar21']  = 0
-
-    ##### Constants #####
-    #earth sidereal year in days 
-    earth_year = 365.256363004
-    # EOBL  : Earth's obliquity, in radians
-    earth_obliquity = 0.409092601        # ecliptic obliquity 84381.406" in radians IAU2009
-    # Full julian date of 2000.0
-    j2000_JD = 2451545.0
-    # 1 AU in km
-    km_per_au = 149597870.6996262
-    # solar mass parameter in km^3/s^2:
-    mu = 1.32712440042 * 10**11
-
-    ##### Spice stuff #####
-    metakernel = '/home/nsmith/KRC/kernels/mk/krc_default.tm'
     spice.furnsh(metakernel)
 
     et = spice.datetime2et(epoch_date)
@@ -77,19 +49,35 @@ def get_orbit_params(body_name, body_naifid):
     state_vector = spice.spkezr(str(orbital_id), et, 'ECLIPJ2000', 'NONE', 'SUN')[0]
 
     # get orbital elements of body relative to sun
-    elts = spice.oscelt(state_vector, et, mu)
+    elts = spice.oscelt(state_vector, et, const.mu)
 
     long_of_asc_node    = elts[3]
     eccentricity        = elts[1]
     inclination         = elts[2]
     arg_of_peri         = elts[4]
     mean_anomaly        = elts[5]
-    semimajor_axis      = elts[0] / (1-eccentricity) / km_per_au
+    semimajor_axis      = elts[0] / (1-eccentricity) / const.km_per_au
 
+    return (long_of_asc_node, eccentricity, inclination, arg_of_peri, mean_anomaly, semimajor_axis, epoch_JD)
+
+def get_spin_axis(metakernel, body_naifid, epoch_date):
+    '''
+    Calculates the spin axis using spice kernels.
+    returns tuple of floats:
+    rotation_period:        rotation period [hours]
+    phase_at_j2000:         rotational phase (angle of prime meridian) at J2000 epoch [degrees]
+    pole_ra:                right ascension of spin axis in J2000 frame [radians]
+    pole_dec:               declination of spin axis in J2000 frame [radians]
+    '''    
+    spice.furnsh(metakernel)
+    et = spice.datetime2et(epoch_date)
+
+    #pm: prime meridian
     body_pm = spice.bodvcd(body_naifid, 'PM', 3)[1]
     phase_at_j2000  = body_pm[0]
     rotation_rate   = body_pm[1]
 
+    # SIDAY : Rotation period in Earth days.
     rotation_period = (360.*24)/rotation_rate
 
     body_frame_name = spice.cidfrm(body_naifid, 33)[1]
@@ -98,15 +86,40 @@ def get_orbit_params(body_name, body_naifid):
 
     _, pole_ra, pole_dec = spice.recrad(pole_axis_j2000)
 
-    #### Finished with spice kernels. ####
+    return (rotation_period, phase_at_j2000, pole_ra, pole_dec)
 
-    ##### Calculated from inputs #####
-    # PERIOD, OPERIOD   : Period of the orbit (days)
-    orbit_period = semimajor_axis**(1.5) * earth_year
+def get_secondary_orb_params(orb_elems):
+    '''
+    Calculate additional values needed for porb output.
+    These values are all derived from the orbital elements.
+    returns:
+    orbit_period:           Period of the orbit in Earth days
+    perihelion_date:        J2000 date (days past J2000 epoch) of previous perihelion passage
+    centuries_from_j2000:   Time of reference epoch from j2000 epoch, in centuries 
+    '''
+
+    (long_of_asc_node, eccentricity, inclination, arg_of_peri, mean_anomaly, semimajor_axis, epoch_JD) = orb_elems
+
+    # PERIOD, OPERIOD   : Period of the orbit (Earth days)
+    orbit_period = semimajor_axis**(1.5) * const.earth_year
     # TJP   : J2000 Date of previous perihelion
-    perihelion_date = epoch_JD - (mean_anomaly/360.)*orbit_period - j2000_JD
+    perihelion_date = epoch_JD - (mean_anomaly/360.)*orbit_period - const.j2000_JD
     # TC    : time in centuries from reference date (2000.0)
-    centuries_from_j2000 = (epoch_JD - j2000_JD) / (earth_year*100)
+    centuries_from_j2000 = (epoch_JD - const.j2000_JD) / (const.earth_year*100)
+
+    return (orbit_period, perihelion_date, centuries_from_j2000)
+
+def get_secondary_spin_params(orb_elems, spin_axis):
+    '''
+    Derive secondary parameters, relating the spin axis to the orbital reference frame.
+    These can all be derived from existing orbital elements and spin axis parameters.
+    Returns: 
+    obliquity:                      angle between spin axis and orbit pole [radians]
+    rotation_matrix_FtoB:           rotation matrix from orbital frame (F) to seasonal frame (B) [3x3 matrix]
+    true_anomaly_at_vernal_equinox: True anomaly at vernal equinox [radians]
+    '''    
+    (long_of_asc_node, eccentricity, inclination, arg_of_peri, mean_anomaly, semimajor_axis, epoch_JD) = orb_elems
+    (rotation_period, phase_at_j2000, pole_ra, pole_dec) = spin_axis
 
     # AFRM  : rotation matrix from orbital (F) to J2000 (A)
     ## First do rotation from orbital (F) to ecliptic (E)
@@ -114,14 +127,15 @@ def get_orbit_params(body_name, body_naifid):
     #### A = (-node)Z * (-inclination)X * (-argument of periapsis)Z
     rotation_matrix_FtoE = spice.eul2m(-1*long_of_asc_node, -1*inclination, -1*arg_of_peri, 3, 1, 3)
     ## add rotation from ecliptic to A frame, equatorial (J2000)
-    rotation_matrix_FtoA = spice.rotmat(rotation_matrix_FtoE, -1*earth_obliquity, 1)
-
+    rotation_matrix_FtoA = spice.rotmat(rotation_matrix_FtoE, -1*const.earth_obliquity, 1)
+    
     # ZFAXU : orbit pole, Z unit vector, in J2000 (a 3-vector)
     orbit_Z_axis_j2000 = rotation_matrix_FtoA[:,2].copy()
     # ZBAXU : spin axis unit vector in J2000
     spin_axis_j2000 = spice.latrec(1, pole_ra, pole_dec)
-    # BLIP  : angle between spin axis and orbit pole, == obliquity
+    # BLIP  : angle between spin axis and orbit pole, == obliquity [radians]
     obliquity = spice.vsep(orbit_Z_axis_j2000, spin_axis_j2000)
+
     # ZBFXU : spin axis, rotated from j2000 into orbital (F) reference frame
     spin_axis_orbital = np.matmul(rotation_matrix_FtoA.T, spin_axis_j2000)
     # XBFXU : Vernal equinox, along spinAxis cross OrbitPole (orbit pole in F is [0,0,1]) 
@@ -131,78 +145,104 @@ def get_orbit_params(body_name, body_naifid):
     yaxis_season_orbital = np.cross(spin_axis_orbital, vernal_equinox_orbital)
     # BFRM  : rotation matrix from orbital frame (F) to seasonal frame (B)
     rotation_matrix_FtoB = np.vstack((vernal_equinox_orbital,yaxis_season_orbital,spin_axis_orbital))
-    # TAV   : True anomaly at vernal equinox
+    # TAV   : True anomaly at vernal equinox [radians]
     true_anomaly_at_vernal_equinox = np.arctan2(vernal_equinox_orbital[1], vernal_equinox_orbital[0])
 
+    return (obliquity, rotation_matrix_FtoB, true_anomaly_at_vernal_equinox)
 
-    ##### record variables in output dictionary #####
-    out_dict['NAME']        = body_name
-    out_dict['PLANUM']      = body_naifid
-    
-    out_dict['TC']          = centuries_from_j2000
-    out_dict['RODE']        = long_of_asc_node
-    out_dict['CLIN']        = inclination
-    out_dict['ARGP']        = arg_of_peri
-    
-    out_dict['XECC']        = eccentricity
-    out_dict['SJA']         = semimajor_axis
-    out_dict['EOBL']        = earth_obliquity
-    out_dict['ZBAA']        = pole_dec
 
-    out_dict['ZBAB']        = pole_ra
-    out_dict['WDOT']        = rotation_rate
-    out_dict['WO']          = phase_at_j2000
-    out_dict['OPERIOD']     = orbit_period
-    out_dict['TJP']         = perihelion_date
-
-    out_dict['SIDAY']       = rotation_period
-    out_dict['TAV']         = true_anomaly_at_vernal_equinox
-    out_dict['BLIP']        = obliquity
-
-    out_dict['BFRM 1']      = rotation_matrix_FtoB[0][0]
-    out_dict['BFRM 2']      = rotation_matrix_FtoB[1][0]
-    out_dict['BFRM 3']      = rotation_matrix_FtoB[2][0]
-    out_dict['BFRM 4']      = rotation_matrix_FtoB[0][1]
-    
-    out_dict['BFRM 5']      = rotation_matrix_FtoB[1][1]
-    out_dict['BFRM 6']      = rotation_matrix_FtoB[2][1]
-    out_dict['BFRM 7']      = rotation_matrix_FtoB[0][2]
-    out_dict['BFRM 8']      = rotation_matrix_FtoB[1][2]
-    out_dict['BFRM 9']      = rotation_matrix_FtoB[2][2]
-    
-    return out_dict
-
-def format_output(out_dict: dict, verbose=False):
+def get_porb_params(body_name, body_naifid, orb_elems, spin_axis):
     '''
-    Formats variables stored in out_dict into a the Fortran style PORB output.
+    Determines the orbital parameters of a body based on spice kernels.
+    Outputs a dictionary containing all the variables to include in the standard PORB
+    input table for KRC.
+    '''
+    # Unpack input orbital elements, derive secondary orbital parameters
+    (long_of_asc_node, eccentricity, inclination, arg_of_peri, mean_anomaly, semimajor_axis, epoch_JD) = orb_elems
+    (orbit_period, perihelion_date, centuries_from_j2000) = get_secondary_orb_params(orb_elems)
+
+    # Unpack input spin axis parameters, derive secondary spin parameters
+    (rotation_period, phase_at_j2000, pole_ra, pole_dec) = spin_axis 
+    (obliquity, rotation_matrix_FtoB, true_anomaly_at_vernal_equinox) = get_secondary_spin_params(orb_elems, spin_axis)
+    ### Note: theoretically, I think you could input the spin axis as obliquity and true 
+    ### anomaly at vernal equinox, and derive pole_ra and pole_dec and the rotation matrix.
+    ### this would potentially be more in line with how people think about objects with unknown spins.
+    
+    ##### record variables in output dictionary #####
+    out={}
+    out['porb_version']     = const.porb_version
+    out['generation_date']  = datetime.datetime.now().strftime('%Y %b %d %H:%M:%S')
+    out['NAME']             = body_name
+
+    out['PLANUM']           = body_naifid    
+    out['TC']               = centuries_from_j2000
+    out['RODE']             = long_of_asc_node
+    out['CLIN']             = inclination
+    out['ARGP']             = arg_of_peri
+
+    out['XECC']             = eccentricity
+    out['SJA']              = semimajor_axis
+    out['EOBL']             = const.earth_obliquity
+    out['SFLAG']            = const.sflag
+    out['ZBAA']             = pole_dec
+
+    out['ZBAB']             = pole_ra
+    out['WDOT']             = (360.*24)/rotation_period
+    out['WO']               = phase_at_j2000
+    out['OPERIOD']          = orbit_period
+    out['TJP']              = perihelion_date
+
+    out['SIDAY']            = rotation_period
+    out['spar17']           = const.spar17
+    out['TAV']              = true_anomaly_at_vernal_equinox
+    out['BLIP']             = obliquity
+    out['PBUG']             = const.pbug
+
+    out['spar21']           = const.spar21
+    out['BFRM 1']           = rotation_matrix_FtoB[0][0]
+    out['BFRM 2']           = rotation_matrix_FtoB[1][0]
+    out['BFRM 3']           = rotation_matrix_FtoB[2][0]
+    out['BFRM 4']           = rotation_matrix_FtoB[0][1]
+
+    out['BFRM 5']           = rotation_matrix_FtoB[1][1]
+    out['BFRM 6']           = rotation_matrix_FtoB[2][1]
+    out['BFRM 7']           = rotation_matrix_FtoB[0][2]
+    out['BFRM 8']           = rotation_matrix_FtoB[1][2]
+    out['BFRM 9']           = rotation_matrix_FtoB[2][2]
+    
+    return out
+
+def format_output(out: dict, verbose=False):
+    '''
+    Formats variables stored in out into a the Fortran style PORB output.
     Outputs a multiline string. 
     Variable labels are optionally included using the verbose flag. 
     '''
     out_str = ''
     if verbose:
         out_str += f'<--VERSION---> <--generation date->           IPLAN      TC orbit:pole\n'
-        out_str += f'PORB:{out_dict['porb_version']} {out_dict['generation_date']} IPLAN,TC= {out_dict['PLANUM']:5.4g} {out_dict['TC']:7.5g} {out_dict['NAME']}:{out_dict['NAME']}\n'
+        out_str += f'PORB:{out['porb_version']} {out['generation_date']} IPLAN,TC= {out['PLANUM']:5.4g} {out['TC']:7.5g} {out['NAME']}:{out['NAME']}\n'
         out_str += f'     PLANUM             Tc           RODE           CLIN           ARGP\n'
-        out_str += f' {out_dict['PLANUM']:10.7g}     {out_dict['TC']:10.7g}     {out_dict['RODE']:10.7g}      {out_dict['CLIN']:.7E} {out_dict['ARGP']:10.7f}\n'
+        out_str += f' {out['PLANUM']:10.7g}     {out['TC']:10.7g}     {out['RODE']:10.7g}      {out['CLIN']:.7E} {out['ARGP']:10.7f}\n'
         out_str += f'       XECC            SJA           EOBL          SFLAG           ZBAA\n'
-        out_str += f'  {out_dict['XECC']:.7E} {out_dict['SJA']:10.7g}     {out_dict['EOBL']:10.7g}     {out_dict['SFLAG']:10.7g}     {out_dict['ZBAA']:10.7g}\n'
+        out_str += f'  {out['XECC']:.7E} {out['SJA']:10.7g}     {out['EOBL']:10.7g}     {out['SFLAG']:10.7g}     {out['ZBAA']:10.7g}\n'
         out_str += f'       ZBAB           WDOT             WO        OPERIOD            TJP\n'
-        out_str += f' {out_dict['ZBAB']:10.7g}     {out_dict['WDOT']:10.7g}     {out_dict['WO']:10.7g}     {out_dict['OPERIOD']:10.7g}     {out_dict['TJP']:10.7g}\n'
+        out_str += f' {out['ZBAB']:10.7g}     {out['WDOT']:10.7g}     {out['WO']:10.7g}     {out['OPERIOD']:10.7g}     {out['TJP']:10.7g}\n'
         out_str += f'      SIDAY          spare            TAV           BLIP           PBUG\n'
-        out_str += f' {out_dict['SIDAY']:10.7g}     {out_dict['spar17']:10.7g}     {out_dict['TAV']:10.7g}     {out_dict['BLIP']:10.7g}     {out_dict['PBUG']:10.7g}\n'
+        out_str += f' {out['SIDAY']:10.7g}     {out['spar17']:10.7g}     {out['TAV']:10.7g}     {out['BLIP']:10.7g}     {out['PBUG']:10.7g}\n'
         out_str += f'      spare         BFRM 1              2              3              4\n'
-        out_str += f' {out_dict['spar21']:10.7g}     {out_dict['BFRM 1']:10.7g}     {out_dict['BFRM 2']:10.7g}     {out_dict['BFRM 3']:10.7g}     {out_dict['BFRM 4']:10.7g}\n'
+        out_str += f' {out['spar21']:10.7g}     {out['BFRM 1']:10.7g}     {out['BFRM 2']:10.7g}     {out['BFRM 3']:10.7g}     {out['BFRM 4']:10.7g}\n'
         out_str += f'          5              6              7              8         BFRM 9\n'
-        out_str += f' {out_dict['BFRM 5']:10.7g}     {out_dict['BFRM 6']:10.7g}     {out_dict['BFRM 7']:10.7g}     {out_dict['BFRM 8']:10.7g}     {out_dict['BFRM 9']:10.7g}\n'
+        out_str += f' {out['BFRM 5']:10.7g}     {out['BFRM 6']:10.7g}     {out['BFRM 7']:10.7g}     {out['BFRM 8']:10.7g}     {out['BFRM 9']:10.7g}\n'
 
     else:
-        out_str += f'PORB:{out_dict['porb_version']} {out_dict['generation_date']} IPLAN,TC= {out_dict['PLANUM']:5.4g} {out_dict['TC']:7.5g} {out_dict['NAME']}:{out_dict['NAME']}\n'
-        out_str += f' {out_dict['PLANUM']:10.7g}     {out_dict['TC']:10.7g}     {out_dict['RODE']:10.7g}      {out_dict['CLIN']:.7E} {out_dict['ARGP']:10.7f}\n'
-        out_str += f'  {out_dict['XECC']:.7E} {out_dict['SJA']:10.7g}     {out_dict['EOBL']:10.7g}     {out_dict['SFLAG']:10.7g}     {out_dict['ZBAA']:10.7g}\n'
-        out_str += f' {out_dict['ZBAB']:10.7g}     {out_dict['WDOT']:10.7g}     {out_dict['WO']:10.7g}     {out_dict['OPERIOD']:10.7g}     {out_dict['TJP']:10.7g}\n'
-        out_str += f' {out_dict['SIDAY']:10.7g}     {out_dict['spar17']:10.7g}     {out_dict['TAV']:10.7g}     {out_dict['BLIP']:10.7g}     {out_dict['PBUG']:10.7g}\n'
-        out_str += f' {out_dict['spar21']:10.7g}     {out_dict['BFRM 1']:10.7g}     {out_dict['BFRM 2']:10.7g}     {out_dict['BFRM 3']:10.7g}     {out_dict['BFRM 4']:10.7g}\n'
-        out_str += f' {out_dict['BFRM 5']:10.7g}     {out_dict['BFRM 6']:10.7g}     {out_dict['BFRM 7']:10.7g}     {out_dict['BFRM 8']:10.7g}     {out_dict['BFRM 9']:10.7g}\n'
+        out_str += f'PORB:{out['porb_version']} {out['generation_date']} IPLAN,TC= {out['PLANUM']:5.4g} {out['TC']:7.5g} {out['NAME']}:{out['NAME']}\n'
+        out_str += f' {out['PLANUM']:10.7g}     {out['TC']:10.7g}     {out['RODE']:10.7g}      {out['CLIN']:.7E} {out['ARGP']:10.7f}\n'
+        out_str += f'  {out['XECC']:.7E} {out['SJA']:10.7g}     {out['EOBL']:10.7g}     {out['SFLAG']:10.7g}     {out['ZBAA']:10.7g}\n'
+        out_str += f' {out['ZBAB']:10.7g}     {out['WDOT']:10.7g}     {out['WO']:10.7g}     {out['OPERIOD']:10.7g}     {out['TJP']:10.7g}\n'
+        out_str += f' {out['SIDAY']:10.7g}     {out['spar17']:10.7g}     {out['TAV']:10.7g}     {out['BLIP']:10.7g}     {out['PBUG']:10.7g}\n'
+        out_str += f' {out['spar21']:10.7g}     {out['BFRM 1']:10.7g}     {out['BFRM 2']:10.7g}     {out['BFRM 3']:10.7g}     {out['BFRM 4']:10.7g}\n'
+        out_str += f' {out['BFRM 5']:10.7g}     {out['BFRM 6']:10.7g}     {out['BFRM 7']:10.7g}     {out['BFRM 8']:10.7g}     {out['BFRM 9']:10.7g}\n'
 
     return out_str
 
@@ -212,15 +252,21 @@ def main():
 
     body_names      = ['Ceres', 'Mars', 'Deimos'] #, 'Chimaera')
     body_naifids    = [2000001, 499, 402] #, 20000623)
+
+    # epoch at which to calculate orbital params (must be covered by available kernels)
+    epoch_date = datetime.datetime(2025,1,1,0,0,0)
+    metakernel = '/home/nsmith/KRC/kernels/mk/krc_default.tm'
     
     for i in range(len(body_names)):
         body_name = body_names[i]
         body_naifid = body_naifids[i]
 
-        out_dict=get_orbit_params(body_name, body_naifid)
+        orb_elems = get_orbital_elements(metakernel, body_naifid, epoch_date)
+        spin_axis = get_spin_axis(metakernel, body_naifid, epoch_date)
+        out  = get_porb_params(body_name, body_naifid, orb_elems, spin_axis)
 
         print()
-        print(format_output(out_dict), verbose=verbose)
+        print(format_output(out), verbose=verbose)
 
     return
 
