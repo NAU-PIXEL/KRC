8717398e06b064a8fa1ebe62526838ec45918c48
nmsplanets <nathan.smith@nau.edu>
2026-02-18 21:58:30 +0000

    removing unused files and subroutines

--- src/cocodp8.f
+++ src/cocodp8.f
@@ -1,5 +1,5 @@
 C_Title	COCODP8 General coordinate conversion package, many routines. DP
-Contains: COCOCM   COCOMC  COCOSC  COCOCS  COCOMS  COCOSM  COCEMC  COCECM 
+Contains: COCOCS 
 CC_Args
 CC  Argument order is:   INPUTS _space_ OUTPUTS
 CC  Argument names are consistent thru all routines.
@@ -17,6 +17,7 @@ C_History  Hugh_H_Kieffer 82Oct26
 C  85Apr changed entries to separate routines, other trivial changes
 C  85Oct17 Document to NIMS standards
 C 2014mar11  HK  Convert cocosp.f to this  REAL*8 version
+C 2026Feb18  NMS Remove unused subroutines
 C_Pause
 C
 C Routine names: COC456 where:
@@ -37,81 +38,6 @@ C  **  E0 > D0
 C  ** Make similar changes to documentation
 C  Uses 90.E0 rather than PI/2 where no additional conversion is involved,
 C   as this eliminates one roundoff error.
-
-        SUBROUTINE COCOCM (V,   DLAT,DLON,R)    ! CARTESIAN TO MAPPING
-        IMPLICIT NONE
-C_Title COCOCM Coordinate conversion: cartesian to mapping
-C_Args
-        REAL*8  V(3)    ![I] Cartesian vector.
-        REAL*8  DLAT    ![O] Latitude; in degrees.
-        REAL*8  DLON    ![O] West longitude; in degrees.
-        REAL*8  R       ![O] Equatorial radius.
-C_Keys  MATH VECTOR COORDINATE CONVERSION
-C_Desc  The subroutine COCOCM performs coordinate conversion: Cartesian
-C       to mapping.  
-C_Call  COCOCS COCOSM
-C_Hist  85Oct17 H.Kieffer, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C_Pause
-        REAL*8 P,Q            ! Internal variables, used as arguments in calls
-C
-        CALL COCOCS (V,   P,Q,R)        ! To spherical
-        CALL COCOSM (P,Q,   DLAT,DLON)  ! Then to mapping angles
-C
-        RETURN
-        END
-
-
-        SUBROUTINE COCOMC (DLAT,DLON,R,    V)   ! MAPPING TO CARTESIAN
-        IMPLICIT NONE
-C_Title  COCOMC Coordinate conversion: mapping to cartesian
-C_Args
-        REAL*8 DLAT     ! [I] Latitude; in degrees
-        REAL*8 DLON     ! [I] West longitude; in degrees
-        REAL*8 R        ! [I] Spherical radius
-        REAL*8 V(3)     ! [O] Cartesian vector
-C_Keys  MATH  VECTOR  COORDINATE  CONVERSION
-C_Desc  Part of COCOSP family, which see
-C_Hist  85Oct17  H.Kieffer, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C       89feb03  HHK revise to ISIS standards.
-C_Paus
-        REAL*8 Q,H,RLAT,RPD             ! Internal variables
-        PARAMETER (RPD=.01745329251994329577D0)  ! Radians per degree
-C
-        RLAT = DLAT*RPD
-        Q = -DLON*RPD           ! Lambda
-        H = R*DCOS(RLAT)                ! Radius of rotation
-        V(1) = H*DCOS(Q)
-        V(2) = H*DSIN(Q)
-        V(3) = R*DSIN(RLAT)
-C
-        RETURN
-        END
-
-
-
-        SUBROUTINE COCOSC ( P,Q,R,   V)         ! SPHERICAL TO CARTESIAN
-        IMPLICIT NONE
-C_Title COCOSC Coordinate conversion: spherical to cartesian
-C_Args
-        REAL*8  P   ![I] Phi = polar angle from Z axis (co-latitude); in radians.
-        REAL*8  Q   ![I] Lambda = azimuthal angle, positive from X axis; in radians.
-        REAL*8  R   ![I] Equatorial radius.
-        REAL*8  V(3)    ![O] Cartesian vector.
-C_Keys  MATH VECTOR COORDINATE CONVERSION
-C_Desc  The subroutine COCOSC performs coordinate conversion: spherical
-C       to Cartesian.
-C_Call  0
-C_Hist  85Oct17 H.Kieffer, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C_Paus
-        REAL*8 H                        ! Internal variables
-C
-        H = R*DSIN(P)   ! Radius of rotation around Z
-        V(1)=H*DCOS(Q)
-        V(2)=H*DSIN(Q)
-        V(3)=R*DCOS(P)
-C
-        RETURN
-        END
 
 
         SUBROUTINE COCOCS (V,   P,Q,R)          ! CARTESIAN TO SPHERICAL
@@ -148,135 +74,3 @@ C
         END
 
 
-        SUBROUTINE COCOMS (DLAT,DLON,  P,Q)     ! MAPPING TO SPHERICAL ANGLES
-        IMPLICIT NONE
-C_Title COCOMS Coordinate conversion: mapping to spherical angles
-C_Args
-        REAL*8  DLAT    ![I] Latitude; in degrees.
-        REAL*8  DLON    ![I] West longitude; in degrees.
-        REAL*8  P       ![O] Phi = polar angle from Z axis (co-latitude); in radians.
-        REAL*8  Q       ![O] Lambda = azimuthal angle, positive from X axis; in radians.
-C_Keys  MATH COORDINATE CONVERSION
-C_Desc  The subroutine COCOMS performs coordinate conversion: mapping
-C       to spherical angles.  
-C_Call  0
-C_Hist  85Oct17 H.Kieffer, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C_Paus
-        REAL*8 RPD                      ! Internal variables
-        PARAMETER (RPD=.01745329251994329577D0)  ! Radians per degree
-C
-        P = (90.D0-DLAT)*RPD
-        Q = -DLON*RPD
-C
-        RETURN
-        END
-
-
-        SUBROUTINE COCOSM (P,Q,   DLAT,DLON)    ! SPHERICAL TO MAPPING ANGLES
-        IMPLICIT NONE
-C_Title COCOSM Coordinate conversion: spherical to mapping angles
-C_Args
-        REAL*8 P   ![I] Phi = polar angle from Z axis (co-latitude); in radians.
-        REAL*8 Q   ![I] Lambda = azimuthal angle, positive from X axis; in radians.
-        REAL*8 DLAT ![O] Latitude; in degrees.
-        REAL*8 DLON ![O] West longitude; in degrees.
-C_Keys  MATH COORDINATE CONVERSION
-C_Desc  The subroutine COCOSM performs coordinate conversion: spherical to
-C       mapping angles.  
-C_Call  0
-C_Hist  85Oct17 H.Kieffer, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C_Paus
-        REAL*8 DPR
-        PARAMETER (DPR=57.295779513082320876798D0)
-C
-        DLAT = 90.D0-P*DPR
-        DLON = -Q*DPR
-C
-        RETURN
-        END
-
-
-        SUBROUTINE COCEMC (DLAT,DLON,R,E,   V)  ! ELLIP. MAPPING TO CARTESIAN
-        IMPLICIT NONE
-C_Title COCEMC Coordinate conversion: ellip. mapping to cartesian
-C_Args
-        REAL*8  DLAT    ![I] Latitude; in degrees.
-        REAL*8  DLON    ![I] West longitude; in degrees.
-        REAL*8  R       ![I] Equatorial radius.
-        REAL*8  E       ![I] Ellipticity of oblate
-                        ! spheroid = sqrt(1.-(R_pole /R_equat)**2).
-        REAL*8  V(3)    ![O] Cartesian vector.
-C_Keys  MATH VECTOR COORDINATE CONVERSION
-C_Desc  The subroutine COCEMC performs coordinate conversion:  Elliptical 
-C       mapping to Cartesian.  
-C_Call  0
-C_Hist  85Oct17 H.Kieffer, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C_End
-        REAL*8 F,G,H,RLAT,RPD,Q         ! Internal variables
-        PARAMETER (RPD=.01745329251994329577D0)  ! Radians per degree
-C
-        RLAT = DLAT*RPD         ! Latitude in radians
-        F = DSIN(RLAT)          ! Sine of latitude
-        G = R/DSQRT(1.D0-(E*F)**2) ! Radius of meridian curvature
-        H = G*DCOS(RLAT)                ! Radius of rotation
-        V(3)=G*F*(1.D0-E**2)
-        Q = -DLON*RPD           ! Lambda = azimuth in radians
-        V(1) = H*DCOS(Q)
-        V(2) = H*DSIN(Q)
-C
-        RETURN
-        END
-
-
-        SUBROUTINE COCECM (V,E,DLAT,DLON,R)     !CARTESIAN TO ELLIP. MAPPING
-        IMPLICIT NONE
-C_Title COCECM Coordinate conversion: cartesian to ellip. mapping
-C_Args
-        REAL*8 V(3)       ! [I] Cartesian vector
-        REAL*8 E          ! [I] Ellipticity of oblate spheroid
-                          !     sqrt(1.-(R_pole/R_equat)**2)
-        REAL*8 DLAT       ! [O] Latitude; in degrees
-        REAL*8 DLON       ! [O] West longitude; in degrees
-        REAL*8 R          ! [O] Equatorial radius.
-C_Keys  MATH VECTOR COORDINATE CONVERSION
-C_Desc  COCECM shall be part of the COCOSP family of routines.  COCECM shall
-C       convert cartesian coordinates to elliptical mapping coordinates.
-C_Hist  86May22 D. Cook, U.S.G.S.,Flagstaff. See COCOSP.FOR
-C_End
-        REAL*8 TINY,TINY2,EVAL,F,G,H    ! Internal variables
-        REAL*8 RLAT,RPD,Q,RG,XU,YU,ZU   ! More internal variables
-        PARAMETER (RPD=.01745329251994329577D0)  ! Radians per degree
-        PARAMETER (TINY=2.D-7)          ! Precision limit
-        PARAMETER (TINY2=1.D-37)        ! Dynamic range limit
-C
-        RG = DSQRT(V(1)**2+V(2)**2+V(3)**2)      ! Geocentric radius
-          IF (RG.LT.TINY2) THEN                 ! Too small for this precision
-              DLAT = 0.                         ! Arbitrarily set output to zero
-              DLON = 0.
-          ELSE
-              XU = V(1)/RG                      ! Normalized cartesian vector
-              YU = V(2)/RG
-              ZU = V(3)/RG
-              H  =  DSQRT(XU**2 + YU**2)         ! Calculate radius of rotation
-              EVAL  =  1.D0 - E*E
-                IF (H.GT.TINY) THEN             ! Not near a pole
-                      IF (ABS(XU).LT.TINY) THEN ! On Y-axis
-                          Q = DSIGN(90.D0*RPD,YU)
-                      ELSE
-                          Q = DATAN2(YU,XU)
-                      END IF
-                    DLON = -Q/RPD
-                    RLAT = DATAN(ZU/(H*EVAL))    ! Latitude in radians
-                    G = H/DCOS(RLAT)             ! Radius of meridian curvature
-                    F = DSIN(RLAT)
-                    R  =  RG*G * DSQRT(1.D0-(E*F)**2) ! Equitorial radius
-                    DLAT = RLAT/RPD
-                ELSE                            ! Point near a pole
-                    R  =  DABS(V(3)) / DSQRT(EVAL)! V(3) must be near identical
-                    DLAT = DSIGN(90.D0,ZU)         ! to polar radius.
-                    DLON = 0.
-                END IF
-          END IF
-C
-        RETURN
-        END
--- src/vadddp8.f
+++ src/vadddp8.f
@@ -1,5 +1,4 @@
-C Routines:  VADD  VCROSS  VDOT  VEQUAL  VMAG  VNEG  VNORM  
-C     here:  VPRF  VPRINT  VSCALE  VSHOW  VSUB  VUNIT 
+C Routines:  VADD  VCROSS  VDOT  VEQUAL  VMAG  VNEG  VSCALE  VSUB 
 C  This is the top of a general  3-dimension vector geometry package.
 C  There are four such files, single and double precision, 3 and  N dimensions
 C This is the double precision, REAL*8, version
@@ -15,6 +14,7 @@ C   91dec27  HK  Add  VMAGN; treat  VADDNDP as primary file
 C 2013jun16  HK  Modify only comment typos and format
 C 2014mar10  HK Make  REAL*8  version.  All routines name the same as Single Prec.
 C 2014jun09 HK Add IMPLICIT NONE to all routines. Add  VPRINT 
+C 2026Feb18 NMS Remove unused subroutines
 
       SUBROUTINE VADD (U,V, W)  !  W =  U +  V
 C_Title  VADD  Add two vectors of dimension 3.   Double precision
@@ -143,70 +143,6 @@ C_Paus
       END
 
 
-      SUBROUTINE VNORM (V, W)   !  W =  V/|V|
-C_Title  VNORM  Normalize a vector of dimension 3.
-      IMPLICIT NONE
-C_Args
-      REAL*8  V(*)              ! [i] vector to be normalized
-      REAL*8  W(*)              ! [o] vector of unit length; may overwrite input
-C_Keys  MATH  VECTOR
-C_Desc  Normalizes a vector of dimension 3 to unit length.  W=V/|V|.
-C_Hist  90dec27  Hugh Kieffer.  See comments in first routine
-C_Paus
-      INTEGER I
-      REAL*8 SUM
-      SUM = 0.D0
-      DO I=1,3
-        SUM = SUM +V(I)**2
-      ENDDO
-      SUM = 1.D0/DSQRT(SUM)
-      DO I=1,3
-        W(I) = SUM * V(I)
-      ENDDO
-      RETURN
-      END
-
-      SUBROUTINE VPRF (ID,V,IOP,FMT)
-C_Title  VPRF  Print a vector of dimension 3 in user format.
-      IMPLICIT NONE
-C_Args
-      CHARACTER*(*) ID          ! [i] character variable identification
-      REAL*8 V(*)               ! [i] vector to print
-      INTEGER  IOP              ! [i] logical unit for printer
-      CHARACTER*(*) FMT         ![i] format to use for vector. this should be only
-C                       the core of the format, e.g.   '8F8.3' 
-C_Keys  MATH  VECTOR
-C_Desc  Prints a vector of dimension 3 to the logical unit specified
-C       and in the format specified (up to 30 characters).
-C_Hist  90dec27  Hugh Kieffer.  See comments in first routine
-C_Paus
-      INTEGER I
-      CHARACTER*60 FMT2
-      FMT2 = '(1X,A,''='',(/1X,' // FMT // '))'
-      WRITE (IOP,FMT2) ID,(V(I),I=1,3)
-      RETURN
-      END
-
-
-      SUBROUTINE VPRINT (ID,V,IOP)
-C_Title  VPRINT print a vector of dimension 3. Many significant figures
-      IMPLICIT NONE
-C_Args
-      CHARACTER*(*) ID          ! [i] character variable identification
-      REAL*8 V(*)               ! [i] vector to print
-      INTEGER  IOP              ! [i] logical unit for printer
-C_Keys  MATH  VECTOR
-C_Desc  Prints a vector of dimension 3 to the logical unit specified
-C_Hist  90dec27  HHKieffer  U.S.G.S.,Flagstaff. see comments in first routine
-C_Paus
-      INTEGER I
-
-      WRITE(IOP,34) ID,(V(I),I=1,3)
- 34   FORMAT('0VPRINTN dump of: ',A,(/5X,3D22.13))
-      RETURN
-      END
-
-
       SUBROUTINE VSCALE (R,V, W) !  W =  R *  V
 C_Title  VSCALE  Multiply a vector of dimension 3 by a constant.
       IMPLICIT NONE
@@ -229,52 +165,6 @@ C_Paus
       END
 
 
-      SUBROUTINE VSHOW (ID,V,IOP)
-C_Title  VSHOW  Print a vector of dimension 3 as cartesion and spherical angles
-      IMPLICIT NONE
-C_Args
-      CHARACTER*(*) ID          ! [i] character variable identification
-      REAL*8 V(3)               ! [i] vector to print
-      INTEGER IOP               ! in. Logical unit. Negative to print col headings
-C_Keys  MATH  VECTOR
-C_Desc  Prints a vector of dimension 3 to logical unit specified
-C_Hist  2013jun16  Hugh Kieffer.  See comments in first routine
-C_Paus
-      INTEGER I,J
-      REAL*8  P      ! Phi = polar angle from Z axis (co-latitude); in radians.
-      REAL*8  Q      ! Lambda = azimuthal angle, positive from X axis; in radians.
-      REAL*8  R      ! Equatorial radius.
-      REAL*8 H,PID2,QD,PD,RAD  ! Internal variables
-      PARAMETER (PID2=1.57079632679489662D0) ! PI/2
-      CHARACTER*60 FMT
-      FMT='(3g13.5,2x,2f8.2,g12.5,1x,A)'
-
-C       CALL COCOCS(V, P,Q,R)   !  colat and  East longitude in radians, length
-C copy cococs.f here and convert to REAL*8
-      H = V(1)**2 + V(2)**2     ! X-Y Radius squared
-      IF (H .EQ. 0.D0) THEN
-        Q = 0.D0                ! On polar axis, azimuth indeterminate
-      ELSE
-        Q = DATAN2 (V(2),V(1))
-      ENDIF
-      R = DSQRT (H + V(3)**2)   ! Radius
-      IF (R .EQ. 0.D0) THEN
-        P = 0.D0                ! Radius is zero, polar angle indeterminate
-      ELSE
-        P = PID2 - DATAN2 (V(3), DSQRT(H) )
-      ENDIF
-C^^^^^^^^^^^^^^^ end of converted cococs
-      RAD=90.D0/PID2            ! a radian in degrees
-      QD=Q*RAD                  ! convert longitude to degrees
-      PD=90.d0 -P*RAD           ! convert co-lat to latitude degrees
-      J=ABS(IOP)                ! logical unit to use
-      IF (IOP.LT.0) WRITE(J,*)'C_END  X          Y           Z'
-     &     ,'            Lat    E.Lon   Radius     What'
-      WRITE (J,FMT) (V(I),I=1,3),QD,PD,R,ID
-      RETURN
-      END
-
-
       SUBROUTINE VSUB (U,V, W)  !  W =  U -  V
 C_Title  VSUB   Find difference of two vectors of dimension 3
       IMPLICIT NONE
@@ -292,25 +182,5 @@ C_Paus
       DO I=1,3
         W(I)=U(I)-V(I)
       ENDDO
-      RETURN
-      END
-
-      SUBROUTINE VUNIT (IAX, U)
-C_Title  VUNIT  Construct unit vector of dimension 3 along one axis 
-      IMPLICIT NONE
-C_Args
-      INTEGER IAX               ! [i] axis along which vector will point.
-      REAL*8  U(*)              ! [o] unit vector along axis.
-C_Keys  MATH  VECTOR
-C_Desc  Creates a unit vector of dimension 3 along a  Cartesian axis.
-C     if  IAX is not within  1:N, then a zero vector is returned.
-C_Hist  90dec27  Hugh Kieffer.  See comments in first routine
-C_End
-      INTEGER I
-
-      DO I=1,3
-        U(I)=0.D0
-      ENDDO
-      IF (IAX.GE.1 .AND. IAX.LE.3) U(IAX)=1.D0
       RETURN
       END
